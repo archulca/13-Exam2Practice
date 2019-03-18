40a78495ffb60ba1c78d9e5adbd30aa7d989a079
Carla Archuleta <archulca@rose-hulman.edu>
2019-03-18 15:36:06 -0400

    DONE #6

--- src/practice_problem3.py
+++ src/practice_problem3.py
@@ -622,7 +622,7 @@ def practice_problem3d(sequence):
         #return -1
 
     ###########################################################################
-    # TODO: 6. Just ABOVE this _TODO_, you should have implemented
+    # DONE: 6. Just ABOVE this _TODO_, you should have implemented
     #     a solution for the   practice_problem3d   function.
     #     Here, put ANOTHER solution, as follows:
     #
